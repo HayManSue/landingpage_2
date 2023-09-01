619564d2431d9aa1631506ba48cf56e3f8529587
Hay Marn Sue <haymarnsue@Kyaws-MacBook-Pro.local>
2023-09-01 13:50:20 +0630

    second project

---
+++ script.js
@@ -0,0 +1,6 @@
+// console.log('hello');
+// const checkbox = document.getElementById('#checkbox');
+// checkbox.addEventListener("click", () =>{
+//     // Toggle Website theme
+//     document.body.classList("dark")
+// })
